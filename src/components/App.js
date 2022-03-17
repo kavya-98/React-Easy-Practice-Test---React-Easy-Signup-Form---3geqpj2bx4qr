01326084b9c34871e6000d923b8fe5c3311cacd8
kavya-98 <76950344+kavya-98@users.noreply.github.com>
2022-03-17 19:25:30 +0530

    commiit

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,31 @@ import '../styles/App.css';
 const App = () => {
   
   return (
+<form id="contact" action="./" method="post">
 
+ 
+<label for="user">user</label>
+<input type="text" autocomplete="user" id="user" name="user" required /><br/>
+
+
+
+<label for="email">email</label>
+<input type="email" autocomplete="email" id="email" name="email" /><br/>
+
+
+
+
+<label for="password">password</label>
+<input type="password" autocomplete="password" id="password" name="password" /><br/>
+
+
+<label for="checkbox">checkbox</label>
+<input type="checkbox" id="consent" name="checkbox"/>
+
+<button type="singUp">singUp</button>
+
+
+</form>
   )
 }
 
